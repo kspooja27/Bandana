13cd798df1359514caf70bdbc53b752c6f3293b6
kspooja27 <kspooja27@gmail.com>
2020-12-30 13:42:11 +0530

    Added fileupload

--- controllers/posts.controller.js
+++ controllers/posts.controller.js
@@ -81,6 +81,7 @@ exports.fetchAll = (req, res) => {
 }
 
 exports.fileTest = (req, res) => {
+    let dummy = 'dummy';
     let file = req.files.image;
     let fileName = file.name;
     let ext = fileName.slice((Math.max(0, fileName.lastIndexOf('.')) || Infinity) + 1);
